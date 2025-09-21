b4b3bb3492764425fb5bb91e085a1c94c03fd176
Avril Peraza <avril2.02006@gmail.com>
2025-09-21 15:50:33 -0300

    cambios de prueba - merge request

--- src/main/java/proyecto/spring/asugestionsocios/repository/ActivityRepository.java
+++ src/main/java/proyecto/spring/asugestionsocios/repository/ActivityRepository.java
@@ -11,7 +11,7 @@ public interface ActivityRepository extends JpaRepository<Activity, Long> {
     @Query(value = "SELECT EXISTS (SELECT 1 FROM activity WHERE name = :name)", nativeQuery = true)
     boolean existsActivitiesByName(String name);
 
-    @Query(value = "SELECT a FROM Activity a " +
+    /*@Query(value = "SELECT a FROM Activity a " +
             "WHERE a.id = :activityId")
-    Activity findActivitiesByDateTimeAfter(Long activityId);
+    Activity findActivitiesByDateTimeAfter(Long activityId);*/
 }
